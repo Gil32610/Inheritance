fb223f98ccd376ef0f7657f459063f78106d5fbb
g-i-l <gilaraujo417@gmail.com>
2023-06-19 09:39:51 -0300

    Inheritance

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //Inheritance basically allows objects to reuse fields and functions from a parent class
+        //such as attributes, constructors and methods
+        Animal animal = new Animal ("Generic Animal", "Huge", 400);
+        doAnimalStuff(animal, "Slow");
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "fast");
+        Dog yorkie = new Dog("Yorkie", 15);
+        doAnimalStuff(yorkie,"fast");
+        Dog retriever = new Dog("Labrador Retriever", 65, "Floppy", "Swimmer");
+        doAnimalStuff(retriever,"slow");
+        Dog wolf = new Dog("Wolf", 40);
+        doAnimalStuff(wolf, "slow");
+        Fish goldie = new Fish("Goldfish", 0.25, 2, 3);
+        doAnimalStuff(goldie, "fast");
+    }
+    public static void doAnimalStuff(Animal animal, String speed){
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
+    }
+}
